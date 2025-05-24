e28c12829ac239108c1b9f5891efe7a17a68fc04
Hanna <hanna.lughammer@gmail.com>
2025-05-24 19:31:09 +0200

    Member 1 Änderung

--- lughammer_strohmayer_android/Main.java
+++ lughammer_strohmayer_android/Main.java
@@ -1,1 +1,8 @@
-public class Main { public static void main(String[] args) { System.out.println(Hello World); } }
+public class Main 
+{ 
+    public static void main(String[] args) 
+    { 
+        System.out.println(Hello World); 
+        System.out.println("Aenderung von Member 1"):
+    } 
+}
