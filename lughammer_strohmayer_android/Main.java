40e929b049714e2bebe4c4a61e15396816578009
unknown <lena.strohmayer@outlook.com>
2025-05-25 09:43:15 +0200

    Bugfix 1.Änderung Member2

--- lughammer_strohmayer_android/Main.java
+++ lughammer_strohmayer_android/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Aenderung von Member1");
+        System.out.println("Aenderung von Member2 zu bugfix");
     }
     public void thisIsANewMethod() {
         System.out.println("Aenderung von Member1")
